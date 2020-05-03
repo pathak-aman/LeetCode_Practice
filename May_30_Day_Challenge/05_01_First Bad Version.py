a583aa29b3cd0e9d011a4ee558d26c8653f0b916
Aman2909 <amanpathak2909@gmail.com>
2020-05-03 16:02:27 +0530

    Update #9

--- May_30_Day_Challenge/05_01_First Bad Version.py
+++ May_30_Day_Challenge/05_01_First Bad Version.py
@@ -1,7 +1,7 @@
 # The isBadVersion API is already defined for you.
-@param version, an integer
-@return a bool
-def isBadVersion(version):
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
 
 class Solution:
     def firstBadVersion(self, n):
