517cb89f2ac981b211499e1353f7f28e74c5eb4f
pathak-aman <amanpathak2909@gmail.com>
2025-03-17 14:18:38 -0400

    added correct code

--- Learning/Graph/L33_Path_min_effort.py
+++ Learning/Graph/L33_Path_min_effort.py
@@ -1,40 +1,32 @@
-# Inituition - Djikstra's 
-# Need to keep popping minimum efforts
-# Need to check while popping if we are at the destination, then no other path exist with less efforts
+# Intuition - we use Dijistka's to keep track of the path with minimum effort. In a heap queue, we traverse by popping minimum effort.
+# We stop when the popped element is the destination, as no more path will have lower effort (same or higher only)
 
 
-from collections import deque
+import heapq
 from math import inf
+from typing import List
 
 class Solution:
-    def shortestPathBinaryMatrix(self, grid) -> int:
-        n = len(grid)
-        src = (0,0)
-        dest = (n-1,n-1)
-        distance = [[inf for _ in range(n)] for _ in range(n)]
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        n, m = len(heights), len(heights[0])
+        efforts = [[inf for _ in range(m)] for _ in range(n)]
+        min_heap = []
+        # (effort, (x,y))
+        heapq.heappush(min_heap, (0, (0,0)))
 
-#       (distance, (x,y))
-        q = deque()
-        q.append((0, (0,0)))
+        delx = [0, 1, 0, -1]
+        dely = [1, 0, -1 ,0]
 
-        delx = [0, +1, 0, -1]
-        dely = [+1, 0, -1, 0]
-        
-        while q:
-            dist, (x, y) = q.popleft()
+        while min_heap:
+            curr_effort, (x,y) = heapq.heappop(min_heap)
+            if (x,y) == (n-1, m-1):
+                return curr_effort
+
             for dx, dy in zip(delx, dely):
-                x_new, y_new = x + dx, y + dy
+                x_new, y_new = x+dx, y + dy
 
-                if 0 <= x_new < n and 0 <= y_new < n and grid[x_new][y_new] == 1 and dist + 1 < dist[x_new][y_new]:
-                    dist[x_new][y_new] = dist+ 1
-                    q.append((dist+ 1, (x_new, y_new)))
-
-                    if dest == (x_new, y_new):
-                        return dist + 1
-        return -1
-    
-
-
-
-
-   
+                # validity check:
+                if 0 <= x_new < n and 0 <= y_new < m and max(curr_effort, abs(heights[x][y] - heights[x_new][y_new])) < efforts[x_new][y_new]:
+                    new_effort = max(curr_effort, abs(heights[x][y] - heights[x_new][y_new]))
+                    efforts[x_new][y_new] =  new_effort
+                    heapq.heappush(min_heap, (new_effort, (x_new, y_new)))
